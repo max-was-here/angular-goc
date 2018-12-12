d2556484bea37a47365de5be0709223e3936badd
mbelangereffenti <mbelanger@effenti.ca>
2018-12-12 00:33:39 -0500

    Removed reactive stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,19 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule, MatInputModule, MatToolbarModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './layout/card/card.component';
 import { ObservableComponent } from './layout/observable/observable.component';
-import { ReactiveComponent } from './layout/reactive/reactive.component';
 import { SimpleComponent } from './layout/simple/simple.component';
 import { NotificationModule } from './modules/notification/notification.module';
 import { DeckOfCardsService } from './services/deck-of-cards.service';
-import { DeckEffects } from './store/effects/deck.effect';
-import { deckReducer } from './store/reducers/deck.reducer';
 
 @NgModule({
   declarations: [
     AppComponent,
     ObservableComponent,
     SimpleComponent,
-    ReactiveComponent,
     CardComponent
   ],
   imports: [
@@ -39,18 +31,6 @@ import { deckReducer } from './store/reducers/deck.reducer';
     MatButtonModule,
     MatInputModule,
     MatToolbarModule,
-
-    // NgRx
-    StoreModule.forRoot(
-      {
-        deck: deckReducer,
-      }
-    ),
-    EffectsModule.forRoot([DeckEffects]),
-    StoreDevtoolsModule.instrument({
-      logOnly: environment.production, // Restrict extension to log-only mode
-      maxAge: 25, // Retains last 25 states
-    }),
   ],
   providers: [
     DeckOfCardsService
